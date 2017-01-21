72d36bce60dfec81a68a6b76db742473e671b886
jrunzer26 <jason.runzer@uoit.net>
2017-01-21 17:38:02 -0500

    added ip route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
+var requestIp = require('request-ip');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.post('/saveIP', function(req, res, next) {
+  console.log('before ip');
+  var savedIP = 1;
+  /*
+  publicIp.v4().then(function(ip) {
+    console.log('test');
+    console.log(ip);
+    var addressInformation = {
+      ipAddress: ip
+    };
+    return res.status(200).send(addressInformation);
+  })
+  .catch(function(err) {
+    return res.status(400).send({ipError: err});
+  });
+  */
+  var ip = requestIp.getClientIp(req);
+  return res.status(200).send({ipAddress: ip});  
 });
 
 module.exports = router;
