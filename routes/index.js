061a3120cca8d0130a49e1872a7ac58d1d573d44
jrunzer26 <jason.runzer@uoit.net>
2017-01-27 22:13:46 -0500

    added initial login page

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,21 @@ var requestIp = require('request-ip');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/index', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 
 router.post('/saveIP', function(req, res, next) {
-  console.log('before ip');
-  var savedIP = 1;
-  /*
-  publicIp.v4().then(function(ip) {
-    console.log('test');
-    console.log(ip);
-    var addressInformation = {
-      ipAddress: ip
-    };
-    return res.status(200).send(addressInformation);
-  })
-  .catch(function(err) {
-    return res.status(400).send({ipError: err});
-  });
-  */
   var ip = requestIp.getClientIp(req);
   return res.status(200).send({ipAddress: ip});  
+});
+
+
+/* Login page. */
+
+router.get('/', function(req, res, next) {
+  return res.render('login', {title: 'GeoSaver Login'});
 });
 
 module.exports = router;
